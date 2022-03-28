8d89412d4b26d9e85e5f44a1d612d5d48169a3e6
bleach86 <josh.leach86@yahoo.com>
2022-03-28 16:41:43 -0500

    update stats

--- ghostVault.py
+++ ghostVault.py
@@ -18,7 +18,7 @@ RPCPORT = 51725
 version = "v0.4"
 
 def rpcproxy():
-    rpcproxy = AuthServiceProxy('http://%s:%s@192.168.12.172:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
+    rpcproxy = AuthServiceProxy('http://%s:%s@127.0.0.1:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
     return rpcproxy
 
 def checkConnect():
