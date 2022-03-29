d848cea3318d6bebfa0f17c951c58de007e41acd
bleach86 <josh.leach86@yahoo.com>
2022-03-28 21:24:59 -0500

    auto update

--- ghostVault.py
+++ ghostVault.py
@@ -15,7 +15,7 @@ RPCUSER = 'user'
 RPCPASSWORD = 'password'
 RPCPORT = 51725
 
-version = "v0.5"
+version = "v0.6"
 
 def rpcproxy():
     rpcproxy = AuthServiceProxy('http://%s:%s@127.0.0.1:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
