255aaee3fc6698df21f5d02a8c4cbe85251d398b
bleach86 <josh.leach86@yahoo.com>
2022-03-28 21:52:29 -0500

    update .gitignore

--- ghostVault.py
+++ ghostVault.py
@@ -18,7 +18,7 @@ RPCPORT = 51725
 version = "v0.6"
 
 def rpcproxy():
-    rpcproxy = AuthServiceProxy('http://%s:%s@127.0.0.1:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
+    rpcproxy = AuthServiceProxy('http://%s:%s@192.168.12.172:%d/' % (RPCUSER, RPCPASSWORD, RPCPORT))
     return rpcproxy
 
 def checkConnect():
