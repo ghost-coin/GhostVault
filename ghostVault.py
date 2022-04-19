037ec48e40ff32c1120c5927d929df4e924c4e5e
bleach86 <josh.leach86@yahoo.com>
2022-04-19 15:39:43 -0500

    update stats

--- ghostVault.py
+++ ghostVault.py
@@ -541,7 +541,7 @@ def getStats(duration="all", days=None):
         timeToFind = convertFromSat(int(getStakingInfo()['netstakeweight'])) / convertFromSat(int(getStakingInfo()['weight'])) * 120
         
         if len(oneStake) != 0:
-            nextReward = (int(oneStake[0]['time']) - tnow) - timeToFind
+            nextReward = timeToFine - (tnow - int(oneStake[0]['time']))
         else:
             nextReward = 0
         
