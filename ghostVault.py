ba71b34f4ca1485795e6e396367ce5f01be23b7a
bleach86 <josh.leach86@yahoo.com>
2022-04-19 16:13:20 -0500

    update stats

--- ghostVault.py
+++ ghostVault.py
@@ -538,7 +538,8 @@ def getStats(duration="all", days=None):
             print(f"Last {i[0]:<9}        {Fore.GREEN}{filter['collated']['records']}                 {filter['collated']['total_reward']}{Style.RESET_ALL}")
             
         oneStake = rpcproxy().filtertransactions({"count":1,"category":"stake","include_watchonly":True})
-        if int(getStakingInfo()['weight'] != 0:
+        
+        if int(getStakingInfo()['weight']) != 0:
             timeToFind = convertFromSat(int(getStakingInfo()['netstakeweight'])) / convertFromSat(int(getStakingInfo()['weight'])) * 120
         else:
             timeToFind == 1
