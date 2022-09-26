0e55a80f291fe4202acd1e78da10d8ccf41c0798
bleach86 <14421739+bleach86@users.noreply.github.com>
2022-09-26 13:19:59 -0500

    update electrumx script

--- electrumxwithghsotvaultinstall.sh
+++ electrumxwithghsotvaultinstall.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo apt update && sudo apt upgrade -y && sudo apt install python3-uvloop
 
-cd ~/GhostVault && python3 ghostVault.py enableelectrumx && cd
+cd ~/GhostVault && python3 ghostVault.py update && python3 ghostVault.py enableelectrumx && cd
 
 wget https://raw.githubusercontent.com/bleach86/electrumx-installer/master/bootstrap.sh -O - | bash
 
